c5cee76280aa7a7159bf9f68e274878ae2828a87
Yanni8 <99135388+Yanni8@users.noreply.github.com>
2022-06-12 14:45:45 +0200

    Add disclaimer

---
+++ src/scripts/input-validator.js
@@ -0,0 +1,19 @@
+
+//Validates the input field for Vanity-BTC
+function inputValidatorBTC(target){
+    target.target.required = true;
+    for(var input of document.getElementsByClassName('vanity-btc-input')){
+        if(input.id != target.target.id){
+            input.value = "";
+            input.required = false;
+        }
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    for(var input of document.getElementsByClassName('vanity-btc-input')){
+        input.addEventListener('input', inputValidatorBTC);
+    }
+});
+
